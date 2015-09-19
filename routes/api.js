6b2438c3eaa8bbd1c0b0e9a63b130cb5aed7fab7
Paul Thompson <paulthom12345@gmail.com>
2015-09-19 20:32:55 +1000

    Finishing for the night

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,12 @@ router.get('/podcast/:id', function (req, res, next) {
   });
 });
 
+router.put('/podcast/:id', function (req, res, next) {
+
+});
+
+router.post('/podcast', function (req, res, next) {
+
+});
+
 module.exports = router;
